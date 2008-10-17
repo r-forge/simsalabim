2a184ca56f55c650fb4bbd09cf5ef9ee78d6222b
lukas (Lukas Gudmundsson) <lukas.gudmundsson@geo.uio.no>
2008-10-17 08:14:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simsalabim@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/simsalabim/R/catSSA_KGgap.R
+++ pkg/simsalabim/R/catSSA_KGgap.R
@@ -4,63 +4,71 @@
 ##
 ## Implementation ofthe SSA gapfilling procedure presented by Kondrashov and
 ## Ghil (Nonlin. Processes Geophys., 13, 151–159, 2006)
+##
+## modified
+## 10.10.2008: various bugs and inconsistencies fixed
+## 15.10.2008: updated cross validation.
 ################################################################################
 
-kgSSAM <- function(x,L,toeplitz=FALSE,K,max.inner=10,eps=0.98,getFreq = TRUE){
-### Kondrashov and Ghil SSA for time series with missings
-    if(length(K)==1)
-        K <- 1:K
-    mpos <- is.na(x)
-    x[!mpos]<-x[!mpos]-mean(x,na.rm=TRUE)
-    x[mpos]<- 0
-    for(k in seq(along=K)){
-        for(i in 1:max.inner){
-            x.ssa <- decompSSA(x=x,L=L,toeplitz=toeplitz,getFreq=FALSE)
-            x.RCsum <- reconSSA(dSSA=x.ssa,x=x,groups=list(K[1:k]))
-            if(i==1){
-                R2 <- cor(x.RCsum[mpos],x[mpos])^2
-                x[mpos]<-x.RCsum[mpos]
-                next
-            } else {
-                R2_old <- R2
-                R2 <- cor(x.RCsum[mpos],x[mpos],method = "pearson")^2
-                x[mpos]<-x.RCsum[mpos]
-            }
-            if(R2>eps)
-                break
-        }
-    }
-    x.ssa <- decompSSA(x=x,L=L,toeplitz=toeplitz,getFreq=getFreq)
-    x[mpos]<-NA
-    x.ssa$x <- x
-    x.ssa$RC <- x.RCsum
-    x.ssa$R2 <- R2
-    x.ssa$seriesName <- deparse(substitute(x))
-    x.ssa$call <- match.call()
-    class(x.ssa) <- "kgSSAM"
-    return(x.ssa)
+kgSSAM <- function(x,L,toeplitz=FALSE,K,max.inner=100,eps=0.05,
+                   getFreq = TRUE,verbose=FALSE){
+   mpos <- is.na(x)
+   x[!mpos]<-x[!mpos]-mean(x,na.rm=TRUE)
+   x[mpos]<- 0
+   nrmse <- rep(NA,K)
+   names(nrmse) <- 1:K
+   xRC <- matrix(NA,length(x),K)
+   colnames(xRC) <- paste("RC",1:K,sep="")
+   for(k in 1:K){
+       for(i in 1:max.inner){
+           x.ssa <- decompSSA(x=x,L=L,toeplitz=toeplitz,getFreq=FALSE)
+           xRC[,k] <- reconSSA(dSSA=x.ssa,x=x,groups=list(1:k))
+           nrmse[k] <- sqrt(mean((x[mpos]-xRC[mpos,k])^2))/sqrt(mean(x[mpos]^2))
+           x[mpos] <- xRC[mpos,k]
+           if(nrmse[k]<=eps)
+               break
+       }
+       if(verbose)
+           cat(date(),"comp",k,"of",K,"iter:",i,"\n")
+   }
+   x.ssa <- decompSSA(x=x,L=L,toeplitz=toeplitz,getFreq=getFreq)
+   x.ssa$RC <- xRC
+   x.ssa$nrmse <- nrmse
+   x.ssa$seriesName <- deparse(substitute(x))
+   x.ssa$call <- match.call()
+   class(x.ssa) <- "kgSSAM"
+   return(x.ssa)
 }
 
-CV.kgSSAM <- function(x,L,K_min=1,K_max=10,cv.gap=0.05,runs=30,...){
+cvSSA.kgSSAM <- function(x,L,K=10,cv.crit="bestApprox",cv.gap=0.05,
+                         runs=200,verboseCV=TRUE,...){
 ### Parameter Estimation based on cross validation
-    K <- seq.int(K_min,K_max)
-    rms <- matrix(NA,length(K),runs)
-    nmpos <- !is.na(x)
-    nmpos <- (1:length(x))[nmpos]
-    cvn <- round(cv.gap*length(nmpos))
-    for(r in 1:runs){
-        cat(date(),"loop",r,"of",runs,"\n")
-        for(k in seq_along(K)){
-            sam <- sample(nmpos,cvn)
-            xx <- x
-            xx[sam] <- NA
-            x.ssa <- kgSSAM(xx,L=L,K=K[k],...)
-            rms[k,r] <- mean((x.ssa$RC[sam]-x[sam])^2,na.rm=TRUE)
-        }
-    }
-    colnames(rms) <- paste("run",1:runs,sep="")
-    rownames(rms) <- paste("RC1_",K,sep="")
-    return(rms)
+### values vor cv.crit: "betsApprox" ; "stabSignal"
+   nrmse <- matrix(NA,K,runs)
+   nmpos <- !is.na(x)
+   nmpos <- (1:length(x))[nmpos]
+   cvn <- round(cv.gap*length(nmpos))
+   origKgSSAM <- kgSSAM(x,L=L,K=K,...)
+   bench <- switch(cv.crit,
+                   "stabSignal"=origKgSSAM$RC,
+                   "bestApprox"=matrix(x, ncol=1, nrow=length(x)),
+                   stop("No valid performance criterion")
+                   )
+   for(r in 1:runs){
+       if(verboseCV)
+           cat(date(),"loop",r,"of",runs,"\n")
+       sam <- sample(nmpos,cvn)
+       xx <- x
+       xx[sam] <- NA
+       RC.cv <- kgSSAM(xx,L=L,K=K,...)$RC
+       nrmse[,r] <- sqrt(colMeans((bench[sam,]-RC.cv[sam,])^2))
+   }
+   colnames(nrmse) <- paste("run",1:runs,sep="")
+   rownames(nrmse) <- paste("RC",1:K,sep="")
+   origKgSSAM$CVerror <- nrmse
+   origKgSSAM$CVcall <- match.call()
+   origKgSSAM$class <- c("kgSSAM","cvSSA")
+   return(origKgSSAM)
 }
 
 
